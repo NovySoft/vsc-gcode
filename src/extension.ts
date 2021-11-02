7a0423ea003c8978b9074685e119418b26414791
Levente Novotny <Novotny.Levente@gmail.com>
2021-11-02 20:49:14 +0100

    Add out to gitignore

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,21 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vsc-gcode" is now active!');
+	console.log('"vsc-gcode" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vsc-gcode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from G-code for vsc!');
+	const disposable = vscode.languages.registerHoverProvider('*', {
+		provideHover(document, position, token) {
+			console.log("provideHover");
+			return new vscode.Hover(
+				new vscode.MarkdownString('For *all* TypeScript documents.')
+			);
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	console.log('"vsc-gcode" is deactivated');
+}
